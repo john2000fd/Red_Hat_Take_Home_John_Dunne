0beeab1566b3f36c49cfb43994f654dd24e60935
u200262 <john.dunne.2020@mumail.ie>
2025-01-27 13:14:18 +0000

    Testing if I am accessing the repository using a GET request and evaluating the status code I received.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-# INITIAL COMMIT
+import requests
+import json
+
+# repository holding the Red Hat build of Keycloak’s container images 
+repository = "https://catalog.redhat.com/software/containers/rhbk/keycloak-rhel9/64f0add883a29ec473d40906"
+
+# Here we are doing a GET request to the red hat Keycloak repo
+repo_Response = requests.get(repository)
+
+# Tsting to make sure we are getting a successful call
+# As the status code we receive is 200, we know the server successfully processed the request  
+print(repo_Response.status_code)
 
 
