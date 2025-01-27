ea06c1b0ddbef4153c2be9a2ef45a317905214b1
u200262 <john.dunne.2020@mumail.ie>
2025-01-27 15:14:55 +0000

    API access issue resolved by using syntax from the Red Hat API public documentation

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import requests
 import json
 
-# repository holding the Red Hat build of Keycloak’s container images 
-repository = "https://catalog.redhat.com/software/containers/rhbk/keycloak-rhel9/64f0add883a29ec473d40906"
+# repository holding the Red Hat build of Keycloak’s container image history 
+repository = "https://catalog.redhat.com/api/containers/v1/repositories/registry/registry.access.redhat.com/repository/rhbk/keycloak-rhel9/images"
 
 # Here we are doing a GET request to the red hat Keycloak repo
 repo_Response = requests.get(repository)
 
-# Tsting to make sure we are getting a successful call
+# Testing to make sure we are getting a successful call
 # As the status code we receive is 200, we know the server successfully processed the request  
 print(repo_Response.status_code)
+
+
+repo_info = requests.get(repository)
+var = json.loads(repo_info.text)
+print(var)
+
+
+    
 
 
